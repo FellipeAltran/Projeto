462d6e68f39e31ceb531b73eb2e081baf17f709a
FellipeAltran <altranfellipe@gmail.com>
2022-09-23 11:42:47 -0300

    segundoCommit

--- Objeto/evitandomodificações.js
+++ Objeto/evitandomodificações.js
@@ -1,6 +1,6 @@
 // Object.preventExtensions
-// nao deixa que seja adicionado nada ao objeto, mas permite remoções
-// isExtensible testa se um objeto foi feito com o prevents
+// nao deixa que seja adicionado nada ao objeto, mas permite remoções e alterações
+// isExtensible verifica se um objeto foi feito com o prevents
 
 const produto = Object.preventExtensions({
     nome: 'Fellipe',
@@ -9,4 +9,25 @@ const produto = Object.preventExtensions({
 console.log('Extensivel:', Object.isExtensible(produto))
 
 produto.nome = 'Altran'
-produto.descricao = 'estudante'
+produto.descricao = 'estudante' 
+delete produto.idade
+console.log(produto)
+
+// Object.seal
+// neste comando voce nao consegue adcionar nem remover nada de um objeto apenas modificar
+// isSealed verifica se um objeto foi feito com o seal
+
+const pessoa = Object.seal({ 
+    nome: 'Fernandes',
+    olho: 'castanho'
+})
+
+console.log('Extensivel:', Object.isSealed(produto))
+
+pessoa.sobrenome = 'alteran'
+pessoa.olho = 'preto'
+
+console.log(pessoa)
+
+// Object.freeze = selado + valores constantes
+
